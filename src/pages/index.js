e7c45a297fe9e89a82454f66bc3f4e571a9f1d5e
Tillmann Hübner <ruohki@gmail.com>
2018-11-07 23:19:12 +0100

    audio mute button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { setConfig } from 'react-hot-loader';
+import styled from 'styled-components';
+
+import { VolumeNormal, VolumeMuted } from '../components/svg';
 
 import Layout from '../components/layout';
 import Fader from '../components/downfader';
@@ -9,19 +12,40 @@ import BGMusic from '../audio/bg.webm';
 
 setConfig({ pureSFC: true });
 
-        
+const Mute = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+
+  color: white;
+  svg path {
+    transition: color 5s;
+    color: ${({ muted }) =>  muted ? 'rgba(255,255,255, 1)' : 'rgba(255,255,255,.25)'};
+  }
+`
 
 const IndexPage = () => {
   const player = useRef(null);
-
+  const [ muted, setMuted ] = useState(true);
+  
   useEffect(() => {
-    player.current.play()
-  }, [player]);
+    player.current.muted = muted;
+  }, [ muted ])
 
   return (
     <Layout>
+      <Mute muted={muted} onClick={() => setMuted(!muted)}>
+        {muted ? (
+          <VolumeMuted /> 
+        ) : (
+          <VolumeNormal />
+        )}
+      </Mute>
       <h1><span style={{ fontWeight: 100}}>Niemals vergessen, </span> <Fader list={List} /></h1>
-      <audio ref={player} src={BGMusic} loop />
+      <audio ref={player} src={BGMusic} loop muted autoPlay />
     </Layout>
   )
 }
